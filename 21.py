9b0a9f10799470bcd759c4f5f23f5c0fd1a62ddf
Matthias Volk <mv@infrablue.org>
2023-01-02 21:58:51 +0100

    Day 21

---
+++ 21.py
@@ -0,0 +1,65 @@
+from itertools import cycle, product
+from functools import cache
+
+INPUT_FILE = "input_21"
+
+def mmod(m, n):
+    if m % n == 0:
+        return n
+    else:
+        return m % n
+
+def play_round(pos_a, score_a, pos_b, score_b, dice, k):
+    n1, n2, n3 = dice(), dice(), dice()
+    k += 3
+
+    pos_a = mmod(pos_a + n1 + n2 + n3, 10)
+    score_a = score_a + pos_a
+
+    return pos_a, score_a, pos_b, score_b, k
+
+@cache
+def num_wins(pos_a, score_a, pos_b, score_b):
+    wins_a = 0
+    wins_b = 0
+
+    for n1, n2, n3 in product(range(1, 4), repeat=3):
+        pos_a_new = mmod(pos_a + n1 + n2 + n3, 10)
+        score_a_new = score_a + pos_a_new
+
+        if score_a_new >= 21:
+            wins_a += 1
+        else:
+            wb, wa = num_wins(pos_b, score_b, pos_a_new, score_a_new)
+            wins_a += wa
+            wins_b += wb
+
+    return wins_a, wins_b
+
+if __name__ == "__main__":
+    with open(INPUT_FILE) as f:
+        initial_pos_a = int(next(f).strip()[-1])
+        score_a = 0
+        initial_pos_b = int(next(f).strip()[-1])
+        score_b = 0
+
+    pos_a = initial_pos_a
+    pos_b = initial_pos_b
+
+    detdice = cycle(range(1, 101))
+    dice = lambda: next(detdice)
+
+    k = 0
+    while True:
+        pos_a, score_a, pos_b, score_b, k = play_round(pos_a, score_a, pos_b, score_b, dice, k)
+        if score_a >= 1000:
+            print(k * score_b)
+            break
+
+        pos_b, score_b, pos_a, score_a, k = play_round(pos_b, score_b, pos_a, score_a, dice, k)
+        if score_b >= 1000:
+            print(k * score_a)
+            break
+
+    # Part 2
+    print(num_wins(initial_pos_a, 0, initial_pos_b, 0))
